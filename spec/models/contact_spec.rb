0895aee8e19e7a286ceedd6c53fa7011b6632fb6
David Fisher <tibbon@gmail.com>
2014-02-10 10:17:17 -0500

    validates presence of first and last name for contact

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Contact do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without a first name" do
+    expect(Contact.create(last_name: "Fisher", phone: "555-1212")).to_not be_valid
+  end
+
+  it "is invalid without a last name"  do
+    expect(Contact.create(first_name: "David", phone: "555-1212")).to_not be_valid
+  end
 end
