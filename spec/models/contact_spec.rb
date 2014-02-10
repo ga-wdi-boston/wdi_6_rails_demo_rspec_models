00b268bc36182a97353adc9dde20616908fa833f
David Fisher <tibbon@gmail.com>
2014-02-10 12:29:15 -0500

    Refactored validations using Shoulda

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,20 +1,12 @@
 require 'spec_helper'
 
 describe Contact do
-  it "is invalid without a first name" do
-    expect(Contact.create(last_name: "Fisher", phone: "555-1212")).to_not be_valid
-  end
 
-  it "is invalid without a last name"  do
-    expect(Contact.create(first_name: "David", phone: "555-1212")).to_not be_valid
-  end
-
-  it "is invalid without a phone" do
-    expect(Contact.create(first_name: "David", last_name: "Fisher")).to_not be_valid
-  end
-
-  it "is invalid without a birthday" do
-    expect(Contact.create(first_name: "David", last_name: "Fisher", phone: "555-1212")).to_not be_valid
+  describe "validations" do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:phone) }
+    it { should validate_presence_of(:birthday) }
   end
 
   describe "associations" do
