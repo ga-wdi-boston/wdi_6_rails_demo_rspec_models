4649d7e4a468e2124b1168a5639a3159974dd99a
David Fisher <tibbon@gmail.com>
2014-02-10 11:04:18 -0500

    Wrote a class method on Contact called born_on that returns an array of people born on a day of the week

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,13 @@ class Contact < ActiveRecord::Base
   validates :last_name, presence: true
   validates :phone, presence: true
   validates :birthday, presence: true
+
+  def self.born_on(day)
+    # Returns 0-6
+    dow = Date::DAYNAMES.index(day.camelize)
+
+    # Similar to writing:
+    # SELECT * FROM CONTACTS WHERE EXTRACT(DOW FROM birthday) = dow
+    where('EXTRACT(DOW FROM birthday) = ?', dow)
+  end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -16,4 +16,21 @@ describe Contact do
   it "is invalid without a birthday" do
     expect(Contact.create(first_name: "David", last_name: "Fisher", phone: "555-1212")).to_not be_valid
   end
+
+  # All tests for class method of 'born_on'
+  describe ".born_on" do
+    before :each do
+      # Monday birthday people
+      @chris = Contact.create(first_name: "Chris", last_name: "Clearfield", phone: "555-1212", birthday: "2014-02-10")
+      @david = Contact.create(first_name: "David", last_name: "Fisher", phone: "555-1212", birthday: "2014-02-03")
+
+      # Friday birthday people
+      @kelly = Contact.create(first_name: "Kelly", last_name: "Sanders", phone: "555-1212", birthday: "2014-2-07")
+    end
+    it "should return an array of Contacts born on a day of the week" do
+      expect(Contact.born_on("Monday")).to eq [@chris, @david]
+      expect(Contact.born_on("friday")).to eq [@kelly]
+    end
+  end
+
 end
