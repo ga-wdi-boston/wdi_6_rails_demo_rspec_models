ca82f7f7761d195a3c827359f0483530331a2555
David Fisher <tibbon@gmail.com>
2014-02-10 10:22:23 -0500

    Validate presence of birthday for contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,5 @@ class Contact < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :phone, presence: true
+  validates :birthday, presence: true
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -12,4 +12,8 @@ describe Contact do
   it "is invalid without a phone" do
     expect(Contact.create(first_name: "David", last_name: "Fisher")).to_not be_valid
   end
+
+  it "is invalid without a birthday" do
+    expect(Contact.create(first_name: "David", last_name: "Fisher", phone: "555-1212")).to_not be_valid
+  end
 end
